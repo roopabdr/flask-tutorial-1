202126e9226b7930bade7add48a16fd687933693
Roopa Madihally <roopabdr@gmail.com>
2021-01-04 00:31:20 +0530

    passing parameters from app to index page

--- tutorial1.py
+++ tutorial1.py
@@ -2,17 +2,17 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name} !"
+def home(name):
+    return render_template("index.html", content=name, r=2)
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name} !"
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name="Admin"))
 
 if __name__ == "__main__":
     app.run()
