9303c6d746d3c520f67018c91b0b226236615f6a
Roopa Madihally <roopabdr@gmail.com>
2021-01-16 15:03:51 +0530

    flashing messages from one page to another when logging in or logging out

--- tutorial1.py
+++ tutorial1.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "hello" # defining a session with the help of secret key
 app.permanent_session_lifetime = timedelta(minutes=5) # setting permanent sessions - this helps maintain the session even though the browser is closed
-# app.permanent_session_lifetime = timedelta(days=5) # setting permanent sessions
+# app.permanent_session_lifetime = timedelta(days=5) # setting permanent sessions in days
 
 # @app.route("/<name>")
 # def home(name):
@@ -21,9 +21,11 @@ def login():
         session.permanent = True # sets a given or logged in session permanently for no. of days or minutes it has been set to
         user = request.form["nm"]
         session["user"] = user # used for sessions for a given user with naming the dictionary key as 'user"
+        flash("Login Successful !")
         return redirect(url_for("user", usr=user))
     else:
         if "user" in session:
+            flash("Already logged in !")
             return redirect(url_for("user"))
 
         return render_template("login.html")
@@ -33,12 +35,18 @@ def user():
     # verify the user is actully logged in
     if "user" in session:
         user = session["user"] # checking for a particular user with the key as "user"
-        return f"<h1>{user}</h1>" # this is how we're getting data from the session instead of a URL parameter
+        return render_template("user.html", user=user)
+        # return f"<h1>{user}</h1>" # this is how we're getting data from the session instead of a URL parameter
     else:
+        flash("You're not logged in!")
         return redirect(url_for("login"))
 
 @app.route("/logout")
-def logout():
+def logout():    
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been successfully logged out, {user}", "info") # pass a message to be flashed on the login page upon successfully logging out
+    
     session.pop("user", None) # clearing a session on logout
     return redirect(url_for("login"))
 
